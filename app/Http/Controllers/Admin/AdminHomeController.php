f34eb74191b5dde929856e4c04d3e064c2d309ad
Tutku ÇAKIR <webtutkusu@gmail.com>
2026-01-04 10:28:07 +0300

    DashboardGuncellemesi

--- app/Http/Controllers/Admin/AdminHomeController.php
+++ app/Http/Controllers/Admin/AdminHomeController.php
@@ -4,11 +4,21 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Post;
+use App\Models\Comment;
+use App\Models\Reply;
 
 class AdminHomeController extends Controller
 {
     public function index()
     {
-        return view('admin.home');
+        $post_count = Post::count();
+        $pending_comments_count = Comment::where('status',0)->count();
+        $pending_replies_count = Reply::where('status',0)->count();
+
+        return view('admin.home',compact('post_count','pending_comments_count','pending_replies_count'));
     }
+
+
+
 }
--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -3,5 +3,53 @@
 @section('heading', 'Pano')
 
 @section('main_content')
+                <div class="row">
 
+                    <div class="col-lg-4 col-md-6 col-sm-6 col-12">
+                        <div class="card card-statistic-1">
+                            <div class="card-icon bg-danger">
+                                <i class="fas fa-book-open"></i>
+                            </div>
+                            <div class="card-wrap">
+                                <div class="card-header">
+                                    <h4>Toplam Blog Yazısı <a href="{{ route('admin_post_show') }}"><i class="fas fa-arrow-right"></i></a> </h4>
+                                </div>
+                                <div class="card-body">
+                                    {{ $post_count }}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 col-sm-6 col-12">
+                        <div class="card card-statistic-1">
+                            <div class="card-icon bg-warning">
+                                <i class="fas fa-comment"></i>
+                            </div>
+                            <div class="card-wrap">
+                                <div class="card-header">
+                                    <h4>Onay Bekleyen Yorumlar <a href="{{ route('admin_comment_pending') }}"><i class="fas fa-arrow-right"></i></a></h4>
+                                </div>
+                                <div class="card-body">
+                                    {{ $pending_comments_count }}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 col-sm-6 col-12">
+                        <div class="card card-statistic-1">
+                            <div class="card-icon bg-primary">
+                                <i class="far fa-comments"></i>
+                            </div>
+                            <div class="card-wrap">
+                                <div class="card-header">
+                                    <h4>Onay Bekleyen Yanıtlar <a href="{{ route('admin_reply_pending') }}"><i class="fas fa-arrow-right"></i></a></h4>
+                                </div>
+                                <div class="card-body">
+                                    {{ $pending_replies_count }}
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
 @endsection
